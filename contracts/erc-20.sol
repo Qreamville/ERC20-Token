4e192804a13baf4f10abf598a7574c2641ad4d2b
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-26 01:02:58 -1200

    Update erc-20.sol

--- contracts/erc-20.sol
+++ contracts/erc-20.sol
@@ -5,7 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Qream is ERC20, Ownable {
-    constructor() ERC20("Qream", "QRE") {}
+    constructor() ERC20("Qream", "QRE") {
+        _mint(msg.sender, 100 * 10**decimals());
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
